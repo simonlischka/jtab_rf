f1f9b10361004dee5513d5e5b71e567cc83bed62
Simon Lischka <simon@a-studios.org>
2020-09-19 14:38:01 +0200

    Additional Notation characterization tests
    Descriptions still missing, marked with TODOs

--- test/jtab.test.js
+++ test/jtab.test.js
@@ -3,7 +3,7 @@ import jtab from "jtab"; // this monkey patches Raphael and needs to be here
 
 describe("jtab tests", () => {
   describe("raphael", () => {
-    test("should get string number", () => {
+    test("should recognize guitar string number by `$NUMBER` and `$LETTER` syntax", () => {
       let get_string_number = Raphael.fn.get_string_number;
 
       expect(get_string_number("$1")).toBe(1);
@@ -22,27 +22,66 @@ describe("jtab tests", () => {
   });
 
   describe("Notation characterization", () => {
-    describe("with notes of type chords and tabs", () => {
-      test("should be recognized when using all available tab symbols", () => {
+    describe("should recognize chord and tab notation", () => {
+      //TODO: Improve test descriptions
+      test("when containing chords, various tab symbols and multiple bars", () => {
         let tabtype = jtab.characterize(
-          "Bm $3 4 4h5p3h4 5 $2 3 5 7 7h8p7 5/7 | A $4 7 9 $3 7 6 $5 9 $4 7h9 7 $5 9\7 5/7 | "
+          //TODO: Double escaped the back slide here because babel's strict mode raises error. Check if
+          //it still gets parsed correctly - if not find a way to disable strict mode or update the parser.
+          "Bm $3 4 4h5p3h4 5 $2 3 5 7 7h8p7 5/7 | A $4 7 9 $3 7 6 $5 9 $4 7h9 7 $5 9\\7 5/7 | "
         );
 
         expect(tabtype).toBe(1);
       });
 
-      test("should ", () => {
+      test("when containing smaller subset of chords and tabs", () => {
         let tabtype = jtab.characterize("A7 $e 2 3 $B.3.$e.2");
 
         expect(tabtype).toBe(1);
       });
 
-      test("should ", () => {
+      test("when having medium subset of chords and tabs", () => {
         let tabtype = jtab.characterize(
           "Bm $3 4 4h5p3h4 5 $2 3 5 7 7h8p7 5/7 "
         );
 
         expect(tabtype).toBe(1);
+      });
+    });
+
+    describe("should recognize custom chord only notation", () => {
+      test("when containing chord with name", () => {
+        let tabtype = jtab.characterize("%X/X.4/3.2/2.0/0.1/1.0/0[C]");
+
+        expect(tabtype).toBe(2);
+      });
+
+      test("when containing chord without a name", () => {
+        let tabtype = jtab.characterize("%X/X.4/3.2/2.0/0.1/1.0/0");
+
+        expect(tabtype).toBe(2);
+      });
+    });
+
+    describe("should recognize tab only notation", () => {
+      //TODO: Improve test descriptions
+
+      test("when containing some tab 1", () => {
+        var tabtype = jtab.characterize("$1 2 3 $2.3.$1.2");
+
+        expect(tabtype).toBe(3);
+      });
+
+      test("when containing some tab 2", () => {
+        var tabtype = jtab.characterize("$e 2 3 $B.3.$e.2");
+
+        expect(tabtype).toBe(3);
+      });
+
+      test("when containing some tab 3", () => {
+        var tabtype = jtab.characterize("X02220  8.10.10.9.8.8");
+
+        expect(tabtype).toBe(3);
       });
     });
   });
